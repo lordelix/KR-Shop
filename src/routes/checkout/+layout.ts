0a8dab9e6d9abc71e3eb4a66b7fb7ba588515889
Felix Heidecke <felix@heidecke.me>
2026-06-21 21:06:54 +0200

    Refactor ORDER store to use createResettableStore and update load function to reset on null order

--- src/routes/checkout/+layout.ts
+++ src/routes/checkout/+layout.ts
@@ -1,13 +1,12 @@
 import { useOrder } from '$lib/boilerplate/xioni/shop/Order';
 import { ORDER } from '$lib/stores';
-import type { XioniShop } from '$lib/boilerplate/xioni/types';
 
 export const prerender = false;
 
 export async function load() {
   const order = await useOrder().getOrder();
   if (!order) {
-    ORDER.set({ address: {}, total: {}, shippingCost: {} } as XioniShop.Order);
+    ORDER.$reset();
     return;
   }
   ORDER.set(order);
