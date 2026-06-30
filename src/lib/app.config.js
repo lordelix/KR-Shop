976fd7c49d5cbaa98e6499f0e62ac793e14a13a8
Daniel Haus <d@danielhaus.de>
2026-06-30 18:13:35 +0200

    feat: make api base url configurable

--- src/lib/app.config.js
+++ src/lib/app.config.js
@@ -1,7 +1,8 @@
 import { env } from '$env/dynamic/public'
 
 export default {
-	paypalClientId: env.PUBLIC_PAYPAL_CLIENT_ID || '',
+	shopApiBaseUrl: env.PUBLIC_SHOP_API_BASE_URL || '',
 	shopApiKey: env.PUBLIC_SHOP_API_KEY || '',
-	shopModuleId: env.PUBLIC_SHOP_MODULE_ID || 0
+	shopModuleId: env.PUBLIC_SHOP_MODULE_ID || 0,
+	paypalClientId: env.PUBLIC_PAYPAL_CLIENT_ID || ''
 }
